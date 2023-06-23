f8d8f7793e4aed0b7579ef3143b8e7e392dd5bb2
Agustin Parmisano <agustinparmisano@gmail.com>
2023-06-22 23:47:09 -0300

    Subiendo nuevo daemon automatico para que cree la parte de django rest spectacular con tokens jwt andando

--- instituto/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-
-urlpatterns = [
-    ### ADMIN URLS ###
-    path('admin/', admin.site.urls),
-    path('api/', include(('academico.urls','academico'), namespace='api-academico')),
-]
