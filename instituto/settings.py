b31464304543b9703b8c90a44a1ad3d23adc1ae3
m38 <agustinparmisano@gmail.com>
2025-05-12 17:28:26 -0300

    agregando mysql en settings

--- instituto/settings.py
+++ instituto/settings.py
@@ -75,14 +75,28 @@ WSGI_APPLICATION = 'instituto.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',         # Nombre de la base de datos
+        'USER': 'user',            # Usuario definido en MySQL
+        'PASSWORD': 'password',    # Contraseña del usuario
+        'HOST': 'db',              # Nombre del servicio en Docker Compose
+        'PORT': '3306',            # Puerto interno de MySQL en el contenedor
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # Modo estricto de MySQL
+            'charset': 'utf8mb4',  # Codificación recomendada
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
